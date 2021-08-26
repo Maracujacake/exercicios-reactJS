64a993b14eac5b3227ba17eaa8b4f760a30b259d
Chris Maracujá <74564883+Maracujacake@users.noreply.github.com>
2021-08-26 20:12:06 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -12,3 +12,28 @@ const divexemplo = document.getElementById('exemplo');
 ReactDOM.render(<h1>Teste</h1>,divexemplo)
 // por enquanto não é possível usar o JSX (tags html no javascript)
 // para isso iremos inserir o babel
+
+// function component 
+
+/*
+
+    *************** muito importante: nomear a const puxando o elemento do html antes de qualquer coisa
+    const nomeDaDivNoHtml = document.getElementById('exemplo1')
+
+    function NomeDaFunçao(){
+
+        var nome = 'chrisMaracujá'
+
+        return(
+            <div>
+                <p>Texto qualquer texto qualquer meu nome é: {nome}</p>
+            </div>
+        )
+    }
+    
+    ReactDOM.render(<NomeDaFunçao/>,'nomeDaDivNoHtml')
+
+
+    em <NomeDaFunçao /> USAR SEMPRE A PRIMEIRA LETRA COMO MAIUSCULA 
+    PascalCase no nome das funções sempre!!!!!!!
+*/
