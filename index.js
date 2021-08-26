2e122ebc05965dd1735ae1d7f3509be233ef0bf6
Chris Maracujá <74564883+Maracujacake@users.noreply.github.com>
2021-08-26 19:56:49 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ teste()
 */
 
 // pra testar se tava funcionando mesmo rs
-
 const divexemplo = document.getElementById('exemplo');
-ReactDOM.render('exemplotexto',divexemplo)
+ReactDOM.render(<h1>Teste</h1>,divexemplo)
 // por enquanto não é possível usar o JSX (tags html no javascript)
 // para isso iremos inserir o babel
